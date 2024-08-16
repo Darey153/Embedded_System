55caf256ce0837b72f49a5b864481c5d7dcf5a0c
Darey153 <73605904+Darey153@users.noreply.github.com>
2024-08-15 21:10:16 -0400

    Update Part1_LED_Blink_and_Chase.ino

--- Part1_LED_Blink_and_Chase.ino
+++ Part1_LED_Blink_and_Chase.ino
@@ -24,3 +24,10 @@ int main(void)
  }
  return 0;
 }
+/*Part 1: Using an Arduino UNO or Arduino Nano, a breadboard, and an LED, write a program in that will do the following:
+
+You may not use Arduino functions like pinMode, digitalWrite, digitalRead, etc...
+
+Blink the LED 5 times (on for 500 milliseconds and off for 500 milliseconds).
+Blink the LED 5 times (on for 1 second and off for 1 second).
+Repeat forever.*/
